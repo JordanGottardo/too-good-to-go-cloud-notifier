ae5c430a593aa92851a3b6257b6a494de0df7cc7
JordanG <jordangottardo@libero.it>
2021-06-07 19:45:25 +0200

    Added Dockerfile and publish to ECR script

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9
+COPY ./app .
+COPY ./requirements.txt .
+RUN python -m pip install -r ./requirements.txt --target .
+
+CMD python products_server.py --email testmail1244545453@gmail.com --password Password123_
+EXPOSE 50051
---
+++ buildAndPushToEcr.ps1
@@ -0,0 +1,4 @@
+docker build -t too-good-to-go-cloud-notifier .
+docker tag too-good-to-go-cloud-notifier:latest 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier:latest
+aws ecr get-login-password --profile Personal | docker login --username AWS --password-stdin 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier 
+docker push 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier
