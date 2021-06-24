3c1968b4d1a4c134e3f1ddd125d0ec4e20930cd8
JordanG <jordangottardo@libero.it>
2021-06-24 19:51:36 +0200

    Remove duplicate notifications (WIP)

--- app/product.py
+++ app/product.py
@@ -1,8 +1,19 @@
 from store import Store
+from datetime import datetime
 
 class Product:
     def __init__(self, productFromClient):
-        self.itemId = productFromClient["item"]["item_id"]
+        self.id = productFromClient["item"]["item_id"]
         self.price = productFromClient["item"]["price"]["minor_units"]
         self.decimals = productFromClient["item"]["price"]["decimals"]
         self.store = Store(productFromClient["store"])
+        self.createdTime = datetime.now()
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __ne__(self, o):
+        return not self.__eq__(o)
+
+    def __hash__(self):
+        return hash(self.id)
--- app/products_queue.py
+++ app/products_queue.py
@@ -5,6 +5,7 @@ from product import Product
 import products_pb2
 import logging
 from too_good_to_go_client import TooGoodToGoClient
+from datetime import datetime
 
 
 class ProductsQueue():
@@ -13,7 +14,7 @@ class ProductsQueue():
         self.__InitLogging()
         self.logger.info("ProductsQueue constructor")
         self.queue = queue.Queue()
-        self.set = {}
+        self.set = set()
         self.client = tgtgClient
         self.client.AddEventHandler(self.__productsReceivedEventHandler)
 
@@ -21,21 +22,30 @@ class ProductsQueue():
         return self
 
     def _next(self):
-        return self.queue.get()
+        product: Product = self.queue.get()
+        return self.__ToProductResponse(product)
 
     def __next__(self):
         return self._next()
 
-    def add_response(self, productId):
-        self.queue.put(products_pb2.ProductResponse(id=productId))
+    def __ToProductResponse(self, product: Product):
+        return (products_pb2.ProductResponse(id=product.id))
 
     def __productsReceivedEventHandler(self, products: list[Product]):
         for product in products:
-            if (product not in self.set): # TODO also check for timestamp
+            productAlreadyInSet = product in self.set
+            if (not productAlreadyInSet or self.__HoursDifferenceBetween(product.createdTime, datetime.now()) > 1):
                 self.logger.debug(
-                f"__productsReceivedEventHandler {product.itemId}")
-                self.set #TODO add to set
-                self.add_response(product.itemId)
+                    f"__productsReceivedEventHandler {product.id}")
+                if (productAlreadyInSet):
+                    self.set.remove(product)
+                self.set.add(product)
+                self.queue.put(product)
+
+    def __HoursDifferenceBetween(self, olderDate: datetime, newerDate: datetime):
+        minutes = (newerDate - olderDate).total_seconds() / 60
+        self.logger.debug(f"time difference is{minutes}")
+        return minutes
 
     def __InitLogging(self):
         logging.basicConfig(format="%(threadName)s:%(message)s")
