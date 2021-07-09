ee739215e260f5e5c3ae9342dec930cce4d21e86
JordanG <jordangottardo@libero.it>
2021-07-09 12:46:29 +0200

    BuildAndPush: specified region

--- buildAndPushToEcr.ps1
+++ buildAndPushToEcr.ps1
@@ -1,6 +1,8 @@
+$Region = "eu-central-1"
+
 docker build -t too-good-to-go-cloud-notifier .
 docker tag too-good-to-go-cloud-notifier:latest 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier:latest
-aws ecr get-login-password --profile Personal | docker login --username AWS --password-stdin 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier 
+aws ecr get-login-password --profile Personal --region $Region | docker login --username AWS --password-stdin 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier 
 docker push 166718199143.dkr.ecr.eu-central-1.amazonaws.com/too-good-to-go-cloud-notifier
-aws ecs update-service --cluster grpc --service too-good-to-go-cloud-notifier --force-new-deployment --profile Personal
+aws ecs update-service --cluster grpc --service too-good-to-go-cloud-notifier --force-new-deployment --profile Personal --region $Region
 docker image prune -f
