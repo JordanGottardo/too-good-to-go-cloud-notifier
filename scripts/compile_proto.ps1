b667b89014e9bfd36bbe591ad73dda9855a81e4d
JordanG <jordangottardo@libero.it>
2021-07-09 12:28:24 +0200

    Compile protos: fix

--- scripts/compile_proto.ps1
+++ scripts/compile_proto.ps1
@@ -9,4 +9,4 @@ if (Test-Path $ProductsFilePath) {
     Remove-Item $ProductsFilePath
 }
 
-# python -m grpc_tools.protoc -I../app/protos --python_out=../app --grpc_python_out=../app ../app/protos/products.proto
+python -m grpc_tools.protoc -I../app/protos --python_out=../app --grpc_python_out=../app ../app/protos/products.proto
