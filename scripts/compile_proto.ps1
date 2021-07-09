acacfbf016c90e63feb931c2069192f42d64b321
JordanG <jordangottardo@libero.it>
2021-07-09 12:27:18 +0200

    Compile protos: delete old files

--- scripts/compile_proto.ps1
+++ scripts/compile_proto.ps1
@@ -1,1 +1,12 @@
-python -m grpc_tools.protoc -I../app/protos --python_out=../app --grpc_python_out=../app ../app/protos/products.proto
+$ProductsGrpcFilePath = "..\app\products_pb2_grpc.py"
+$ProductsFilePath = "..\app\products_pb2.py"
+
+if (Test-Path $ProductsGrpcFilePath) {
+    Remove-Item $ProductsGrpcFilePath
+}
+
+if (Test-Path $ProductsFilePath) {
+    Remove-Item $ProductsFilePath
+}
+
+# python -m grpc_tools.protoc -I../app/protos --python_out=../app --grpc_python_out=../app ../app/protos/products.proto
