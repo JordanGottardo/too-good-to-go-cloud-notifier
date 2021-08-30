738bcc08b8d40847b93cd1689859fc5d9d0943ca
JordanG <jordangottardo@libero.it>
2021-08-30 17:08:02 +0200

    Added logging

--- app/products_queue_cache.py
+++ app/products_queue_cache.py
@@ -1,9 +1,11 @@
+import logging
+
 
 class ProductsQueueCache():
 
     def __init__(self):
         self.productsQueueDictionary = {}
-     
+
     def Add(self, id, productsQueue):
         self.productsQueueDictionary[id] = productsQueue
 
@@ -17,9 +19,21 @@ class ProductsQueueCache():
         self.productsQueueDictionary[id].RestartMonitoring()
 
     def SoftStopMonitoring(self, id):
-        self.productsQueueDictionary[id].SoftStopMonitoring()
+        if id in self.productsQueueDictionary:
+            self.productsQueueDictionary[id].SoftStopMonitoring()
+        else:
+            self.logger.warn(
+                f"ProductsQueueCache SoftStopMonitoring: No subscription with {id} ID")
 
     def HardStopMonitoring(self, id):
-        self.productsQueueDictionary[id].HardStopMonitoring()
-        self.productsQueueDictionary.pop(id)
+        if id in self.productsQueueDictionary:
+            self.productsQueueDictionary[id].HardStopMonitoring()
+            self.productsQueueDictionary.pop(id)
+        else:
+            self.logger.warn(
+                f"ProductsQueueCache HardStopMonitoring: No subscription with {id} ID")
 
+    def __InitLogging(self):
+        logging.basicConfig(format="%(threadName)s:%(message)s")
+        self.logger = logging.getLogger("ProductsQueueCache")
+        self.logger.setLevel(logging.DEBUG)
