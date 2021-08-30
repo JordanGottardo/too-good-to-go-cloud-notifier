98c0155ab915a61afec0718505520f2244f4a3ed
JordanG <jordangottardo@libero.it>
2021-08-30 17:11:31 +0200

    ProductsQueueCache: added locks

--- app/products_queue_cache.py
+++ app/products_queue_cache.py
@@ -1,37 +1,46 @@
 import logging
+from threading import Lock
 
 
 class ProductsQueueCache():
 
     def __init__(self):
+        self.lock = Lock()
         self.productsQueueDictionary = {}
+        self.__InitLogging()
 
     def Add(self, id, productsQueue):
-        self.productsQueueDictionary[id] = productsQueue
+        with self.lock:
+            self.productsQueueDictionary[id] = productsQueue
 
     def Get(self, id):
-        return self.productsQueueDictionary[id]
+        with self.lock:
+            return self.productsQueueDictionary[id]
 
     def Contains(self, id):
-        return id in self.productsQueueDictionary
+        with self.lock:
+            return id in self.productsQueueDictionary
 
     def RestartMonitoring(self, id):
-        self.productsQueueDictionary[id].RestartMonitoring()
+        with self.lock:
+            self.productsQueueDictionary[id].RestartMonitoring()
 
     def SoftStopMonitoring(self, id):
-        if id in self.productsQueueDictionary:
-            self.productsQueueDictionary[id].SoftStopMonitoring()
-        else:
-            self.logger.warn(
-                f"ProductsQueueCache SoftStopMonitoring: No subscription with {id} ID")
+        with self.lock:
+            if id in self.productsQueueDictionary:
+                self.productsQueueDictionary[id].SoftStopMonitoring()
+            else:
+                self.logger.warn(
+                    f"ProductsQueueCache SoftStopMonitoring: No subscription with {id} ID")
 
     def HardStopMonitoring(self, id):
-        if id in self.productsQueueDictionary:
-            self.productsQueueDictionary[id].HardStopMonitoring()
-            self.productsQueueDictionary.pop(id)
-        else:
-            self.logger.warn(
-                f"ProductsQueueCache HardStopMonitoring: No subscription with {id} ID")
+        with self.lock:
+            if id in self.productsQueueDictionary:
+                self.productsQueueDictionary[id].HardStopMonitoring()
+                self.productsQueueDictionary.pop(id)
+            else:
+                self.logger.warn(
+                    f"ProductsQueueCache HardStopMonitoring: No subscription with {id} ID")
 
     def __InitLogging(self):
         logging.basicConfig(format="%(threadName)s:%(message)s")
